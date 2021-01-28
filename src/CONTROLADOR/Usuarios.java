fc8c68501b273c9f6e995c34c24b764134980a2f
Sebastián Espinel <67613170+Jespinelp@users.noreply.github.com>
2021-01-27 19:30:48 -0500

    Archivo src

---
+++ src/CONTROLADOR/Usuarios.java
@@ -0,0 +1,159 @@
+package CONTROLADOR;
+
+import java.sql.ResultSet;
+
+import MODELO.Conexion;
+
+public class Usuarios {
+	private String User;
+	private String Clave;
+	private String Nombre;
+	private String Apellido;
+
+	public Usuarios(String user, String clave, String nombre, String apellido) {
+
+		this.User = user;
+		this.Clave = clave;
+		this.Nombre = nombre;
+		this.Apellido = apellido;
+
+	}
+
+	public Usuarios() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getClave() {
+		return Clave;
+	}
+
+	public void setClave(String clave) {
+		Clave = clave;
+	}
+
+	public String getUser() {
+		return User;
+	}
+
+	public void setUser(String user) {
+		User = user;
+	}
+
+	public String getNombre() {
+		return Nombre;
+	}
+
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+
+	public String getApellido() {
+		return Apellido;
+	}
+
+	public void setApellido(String apellido) {
+		Apellido = apellido;
+	}
+
+
+	public boolean obtenerUsuario_I(String usu, String contra) {
+
+		boolean respuesta = false;
+		String sentencia = "select usuario.nombre, usuario.clave from usuario as u where u.nombre='" + usu + "' and u.clave= '"
+				+ contra + "';";
+		System.out.print("BIENVENIDO");
+		try {
+			ResultSet rs;
+			Conexion clsCon = new Conexion();
+			rs = clsCon.Consulta(sentencia);
+			if (rs.next()) {
+				respuesta = true;
+				this.setUser(usu);
+				this.setClave(contra);
+			} else {
+				respuesta = false;
+				rs.close();
+			}
+		} catch (Exception ex) {
+			System.out.println("USUARIO O CONTRASEÑA INCORRECTOS");
+		}
+		return respuesta;
+
+	}
+
+	public boolean obtenerUsuario_E(String usu, String contra) {
+
+		boolean respuesta = false;
+		String sentencia = "select usuario.nombre, usuario.clave from usuario as u where u.nombre='" + usu + "' and u.clave='"
+				+ contra + "';";
+		System.out.print(sentencia);
+		try {
+			ResultSet rs;
+			Conexion clsCon = new Conexion();
+			rs = clsCon.Consulta(sentencia);
+			if (rs.next()) {
+				respuesta = true;
+				this.setUser(usu);
+				this.setClave(contra);
+			} else {
+				respuesta = false;
+				rs.close();
+			}
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
+		return respuesta;
+
+	}
+
+	public boolean obtenerUsuario_Do(String usu, String contra) {
+
+		boolean respuesta = false;
+		String sentencia = "select * from usuarios_login as u where u.id_usuario='" + usu + "' and u.contrasenia='"
+				+ contra + "' and role = 'docente';";
+		System.out.print(sentencia);
+		try {
+			ResultSet rs;
+			Conexion clsCon = new Conexion();
+			rs = clsCon.Consulta(sentencia);
+			if (rs.next()) {
+				respuesta = true;
+				this.setUser(usu);
+				this.setClave(contra);
+			} else {
+				respuesta = false;
+				rs.close();
+			}
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
+		return respuesta;
+
+	}
+
+	public boolean obtenerUsuario_Di(String usu, String contra) {
+
+		boolean respuesta = false;
+		String sentencia = "select * from usuarios_login as u where u.id_usuario='" + usu + "' and u.contrasenia='"
+				+ contra + "' and role = 'director';";
+		System.out.print("BIENVENIDO");
+		try {
+			ResultSet rs;
+			Conexion clsCon = new Conexion();
+			rs = clsCon.Consulta(sentencia);
+			if (rs.next()) {
+				respuesta = true;
+				this.setUser(usu);
+				this.setClave(contra);
+			} else {
+				respuesta = false;
+				rs.close();
+			}
+		} catch (Exception ex) {
+			System.out.println("USUARIO O CONTRASEÑA INCORRECTOS");
+		}
+		return respuesta;
+
+	}
+
+}
---
+++ src/MODELO/Conexion.java
@@ -0,0 +1,99 @@
+package MODELO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Conexion {
+	private Statement St;
+	private static String driver;
+	private static String user;
+	private static String pwd;
+	private static String cadena;
+	private Connection con;
+
+	static String getDriver() {
+		return driver;
+	}
+
+	static String getUser() {
+		return user;
+	}
+
+	static String getPwd() {
+		return pwd;
+	}
+
+	static String getCadena() {
+		return cadena;
+	}
+
+	public Connection getConexion() {
+		return this.con;
+	}
+
+	public Conexion() {
+
+		this.driver = "org.postgresql.Driver";
+		this.user = "postgres";
+		this.pwd = "1234";
+		this.cadena = "jdbc:postgresql://localhost:5432/Base_Relacional_PI";
+		this.con = this.crearConexion();
+
+	}
+
+	public static Connection crearConexion() {
+		try {
+			Class.forName("org.postgresql.Driver");
+			System.out.println("Conexion 1: Exitosa");
+		} catch (ClassNotFoundException e) {
+
+		}
+
+			try {
+				Class.forName(getDriver()).newInstance();
+				Connection con = DriverManager.getConnection(getCadena(), getUser(), getPwd());
+				System.out.println("Conexion 2: Exitosa");
+				return con;
+			} catch (Exception ee) {
+				System.out.println("Error: " + ee.getMessage());
+				return null;
+		}
+	}
+
+	public String Ejecutar(String sql) {
+		String error = "";
+		try {
+			St = getConexion().createStatement();
+			St.execute(sql);
+			error = "Datos insertados";
+		} catch (Exception ex) {
+			error = ex.getMessage();
+		}
+		return (error);
+	}
+
+	public ResultSet Consulta(String sql) {
+		String error = "";
+		ResultSet reg = null;
+
+		try {
+			St = getConexion().createStatement();
+			reg = St.executeQuery(sql);
+
+		} catch (Exception ee) {
+			error = ee.getMessage();
+		}
+		return (reg);
+	}
+
+	public void cerrarConexion() {
+		try {
+			con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
